19de044150ff20fb1b42f730198c7f42c707e68f
Tom Reznick <thomas.reznick@gmail.com>
2020-11-05 21:56:26 -0500

    allow spanish

--- hello.go
+++ hello.go
@@ -4,13 +4,18 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
