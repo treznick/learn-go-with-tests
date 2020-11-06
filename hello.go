8cbde788e277cc0b04290ff710ddeb9c21c7eea6
Tom Reznick <thomas.reznick@gmail.com>
2020-11-05 21:59:18 -0500

    refactor

--- hello.go
+++ hello.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
