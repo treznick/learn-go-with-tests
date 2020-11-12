b050d2be8e17639583bfa94a96e0aa912b912ed4
Tom Reznick <thomas.reznick@gmail.com>
2020-11-11 23:32:40 -0500

    handle empty slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -21,8 +21,12 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
