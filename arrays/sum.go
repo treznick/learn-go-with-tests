b8a38acd21df8856c99120193a0bd3fce30b1b7d
Tom Reznick <thomas.reznick@gmail.com>
2020-11-11 23:28:40 -0500

    SumAllTails

--- arrays/sum.go
+++ arrays/sum.go
@@ -16,3 +16,14 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
