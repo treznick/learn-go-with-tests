04595b3921a1b4c9e1b2a0a853fdff205088f986
Tom Reznick <thomas.reznick@gmail.com>
2020-11-11 22:57:47 -0500

    benchmark test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -10,3 +10,9 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
