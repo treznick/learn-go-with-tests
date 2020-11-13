57e051e129fdd4a4be1221f4da6c7f49f8eb8f81
Tom Reznick <thomas.reznick@gmail.com>
2020-11-12 21:30:19 -0500

    moar better tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -23,15 +23,18 @@ func TestArea(t *testing.T) {
 	}
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		checkArea(t, tt.shape, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.want)
+		})
 	}
 }
