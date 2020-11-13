b79bad84f39237afc0377495720b7906ee66b619
Tom Reznick <thomas.reznick@gmail.com>
2020-11-12 21:25:10 -0500

    Refactor: interfaces

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -22,13 +22,15 @@ func TestArea(t *testing.T) {
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		checkArea(t, tt.shape, tt.want)
+	}
 }
