2bbe573965aa5e1754ed49843fc9ac8edd41559c
Tom Reznick <treznick+github@heroku.com>
2020-12-11 14:23:00 -0500

    refactor

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,18 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test."}
+	dictionary := Dictionary{"test": "this is just a test."}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test."
 
+	AssertStrings(t, got, want)
+}
+
+func AssertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
