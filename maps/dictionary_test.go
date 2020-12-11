900dab5b67d1dfcdf853c0e31599385b5edeac81
Tom Reznick <treznick+github@heroku.com>
2020-12-11 14:33:38 -0500

    error handling

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,29 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test."}
 
-	got := dictionary.Search("test")
-	want := "this is just a test."
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test."
 
-	AssertStrings(t, got, want)
+		AssertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		AssertError(t, err, ErrNotFound)
+	})
+}
+
+func AssertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
 
 func AssertStrings(t *testing.T, got, want string) {
