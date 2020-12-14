f29456b62c355689b04d3b330fa2c8842c7c72ac
Tom Reznick <treznick+github@heroku.com>
2020-12-14 14:23:26 -0500

    implement Add

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,6 +19,20 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	AssertStrings(t, got, want)
+}
+
 func AssertError(t *testing.T, got error, want error) {
 	t.Helper()
 	if got == nil {
