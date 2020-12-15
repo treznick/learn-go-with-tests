bfe45bb56a26fbf4bc6f6236bc1e476ed38f326b
Tom Reznick <treznick+github@heroku.com>
2020-12-15 16:37:07 -0500

    Implement error case for Add

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -20,12 +20,25 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		AssertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		AssertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
@@ -42,12 +55,16 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, definition stri
 
 func AssertError(t *testing.T, got error, want error) {
 	t.Helper()
-	if got == nil {
-		t.Fatal("wanted an error but didn't get one")
-	}
-
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+
+	if got == nil {
+		if want == nil {
+			return
+		}
+
+		t.Fatal("wanted an error but didn't get one")
 	}
 }
 
