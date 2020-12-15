7a23d5a07cefe4a7de96f0114460d864c77c8f9d
Tom Reznick <treznick+github@heroku.com>
2020-12-15 16:39:16 -0500

    refactor: use Error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,17 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for.")
-	ErrWordExists = errors.New("this word already exists in this dictionary")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for.")
+	ErrWordExists = DictionaryErr("this word already exists in this dictionary")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
